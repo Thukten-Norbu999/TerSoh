8c061085f71ad9e770c28eaf8385eac68e63e3d7
Thukten Norbu <thuktennorbu33@gmail.com>
2025-06-04 02:10:56 +0800

    V1

--- controllers/transaction.go
+++ controllers/transaction.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"tersoh-backend/config"
 	"tersoh-backend/models"
+	"tersoh-backend/utils"
 )
 
 func CreateTransaction(w http.ResponseWriter, r *http.Request) {
@@ -12,4 +14,10 @@ func CreateTransaction(w http.ResponseWriter, r *http.Request) {
 	json.NewDecoder(r.Body).Decode(&t)
 	models.DB.Create(&t)
 	w.WriteHeader(http.StatusCreated)
+}
+
+func ListTransactions(w http.ResponseWriter, r *http.Request) {
+	var ts []models.Transaction
+	config.DB.Order("created_at desc").Find(&ts)
+	utils.RespondJSON(w, http.StatusOK, ts)
 }
